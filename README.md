bed4a0a2d2084e935b3d6e1dcec3e7c1c389d477
Valtteri Koskivuori <vkoskiv@gmail.com>
2020-11-19 23:49:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Super simple thumbnail generator for my YouTube videos.
+
+Requires imagemagick and ffmpeg to be installed. You probably also need the Helvetica-Bold font. Just tweak the script if it doesn't work!
+
+Usage: `./thumbgen <video file> <your title string>`
+
+Example: `./thumbgen my_cool_vid.mkv Integrating a flux capacitor into our time machine!`
+
+
---
+++ thumbgen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# check args
+if [ $# -lt 2 ]; then
+	echo "Provide the video file and text, bro"
+	exit -1
+fi
+
+thumbstr=$(echo "$*" | cut -d ' ' -f2-)
+echo "$thumbstr"
+
+# Grab a random frame from the video
+TOTAL_FRAMES=$(ffmpeg -i $1 -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep frame | cut -d ' ' -f 2 | sed s/frame=//)
+FPS=$(ffmpeg -i test.mkv -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep "h264" | awk 'FNR==2 {print $0}' | cut -d ' ' -f 13)
+RANDOM_FRAME=$[RANDOM % TOTAL_FRAMES]
+TIME=$((RANDOM_FRAME/FPS))
+ffmpeg -ss $TIME -i $1 -frames:v 1 $RANDOM_FRAME.png 2>&1 > /dev/null
+FILENAME="$RANDOM_FRAME.png"
+
+# Superimpose stuff with imagemagick
+# Add the dark rectangle
+convert $FILENAME -strokewidth 0 -fill "rgba(0, 0, 0, 0.85)" -draw "rectangle 0,0 960,1080" temp.png
+
+convert temp.png \( -gravity Center -pointsize 180 -size 900x -background transparent -fill white -font helvetica-bold caption:"$thumbstr" \) -gravity West -geometry +25+0 -composite thumbnail.png
+rm temp.png
+rm $RANDOM_FRAME.png
