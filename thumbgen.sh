97bac7b7a664665d94e5e1de3f9dce733286aa29
Valtteri Koskivuori <vkoskiv@gmail.com>
2020-11-20 01:51:23 +0200

    Switch to using ffprobe for getting the FPS value.

--- thumbgen.sh
+++ thumbgen.sh
@@ -11,7 +11,7 @@ echo "$thumbstr"
 
 # Grab a random frame from the video
 TOTAL_FRAMES=$(ffmpeg -i $1 -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep frame | cut -d ' ' -f 2 | sed s/frame=//)
-FPS=$(ffmpeg -i test.mkv -vcodec copy -acodec copy -f null /dev/null 2>&1 | grep "h264" | awk 'FNR==2 {print $0}' | cut -d ' ' -f 13)
+FPS=$(ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=avg_frame_rate $1 | cut -d'/' -f 1)
 RANDOM_FRAME=$[RANDOM % TOTAL_FRAMES]
 TIME=$((RANDOM_FRAME/FPS))
 ffmpeg -ss $TIME -i $1 -frames:v 1 $RANDOM_FRAME.png 2>&1 > /dev/null
